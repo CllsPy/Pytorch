3864c6ff9d72bed15a648e18023a6191b051dd81
Cllspy <ifal.edu.2017@gmail.com>
2025-08-11 16:11:41 -0300

    update

---
+++ test.py
@@ -0,0 +1,13 @@
+import torch
+
+# device = "cuda" if not torch.cuda.is_available() else "cpu"
+
+my_tensor = torch.tensor([[1, 2], [3, 4]], dtype=torch.float32,
+                         device="cpu")
+
+print(my_tensor, my_tensor.dtype, my_tensor.device,
+      my_tensor.shape)
+
+## other initialization methods
+x = torch.empty(size = (3, 3 ))
+print(x, x.device)
